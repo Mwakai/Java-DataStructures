cad1ed834c2ec01756943920268919bf4ecbc860
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-10-21 12:50:55 +0300

    init

--- src/main/java/com/alvin/TheArrays.java
+++ src/main/java/com/alvin/TheArrays.java
@@ -1,5 +1,7 @@
 package com.alvin;
 
+import java.util.Arrays;
+
 public class TheArrays {
 
     public static void main(String[] args) {
@@ -7,6 +9,34 @@ public class TheArrays {
         colors[0] = "purple";
         colors[1] = "blue";
 
-        System.out.println(colors);
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        colors[2] = "yellow";
+        System.out.println(Arrays.toString(colors));
+
+
+        System.out.println(colors.length);
+
+        //Looping arrays
+        for (String s : colors) {
+            System.out.println(s);
+        }
+
+        for (int i =colors.length -1; i>= 0; i--) {
+            System.out.println(colors[i]);
+        }
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
+
     }
 }
