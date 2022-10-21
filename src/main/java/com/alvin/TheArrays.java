18f2c52cdc01f610092b98feefaf7fe570ac6781
Alvin Mwakai <mwakaimwambala@gmail.com>
2022-10-21 12:10:28 +0300

    Initial commit

---
+++ src/main/java/com/alvin/TheArrays.java
@@ -0,0 +1,12 @@
+package com.alvin;
+
+public class TheArrays {
+
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(colors);
+    }
+}
